f79c2b21f691ac120e23cb609dee8a7e0e922877
Alan Gibson <alangibson27@gmail.com>
2015-01-13 22:12:52 +0000

    Temp change to log Mandrill responses

--- app.py
+++ app.py
@@ -46,6 +46,10 @@ def forward(uuid):
                'text': request.form['message'],
               }
     result = mandrill_client.messages.send(message=message)
+    print(result[0]['status'])
+    print(result[0]['code'])
+    print(result[0]['name'])
+    print(result[0]['message'])
     if result[0]['status'] != 'sent':
         abort(500)
     return 'Your message was sent successfully'
