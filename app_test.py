598f9ee1fe6785beef8945d3a60f05b7ff2a7aa2
Alan Gibson <alangibson27@gmail.com>
2015-01-26 21:16:17 +0000

    Added tests for registration and forwarding.

--- app_test.py
+++ app_test.py
@@ -1,5 +1,8 @@
+# coding=UTF-8
+
 import unittest
-from app import app,db
+import flask
+from app import app,db,User
 from flask.ext.testing import TestCase
 
 class AppTest(TestCase):
@@ -22,6 +25,29 @@ class AppTest(TestCase):
         registration_request = {'email': 'testuser@example.com'}
         response = self.client.post('/register', data = registration_request)
         self.assert403(response)
+
+    def test_send_message(self):
+        user = User('alangibson27@gmail.com')
+        db.session.add(user)
+        db.session.commit()
+
+        form_data = {'name': 'アラン',
+                     'email': 'alangibson27@gmail.com',
+                     'lessonType': 'レッスン',
+                     'otherField': 'otherField',
+                     'message': 'アラン'}
+        response = self.client.post('/user/%s' % user.uuid, data = form_data)
+        self.assert200(response)
+
+    def test_send_message_wrong_user(self):
+        form_data = {'name': 'アラン',
+                     'email': 'alangibson27@gmail.com',
+                     'lessonType': 'レッスン',
+                     'otherField': 'otherField',
+                     'message': 'アラン'}
+        response = self.client.post('/user/z', data = form_data)
+        self.assert_status(response, 406)
+
 
 if __name__ == '__main__':
     unittest.main()
